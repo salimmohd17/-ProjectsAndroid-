a34408d5452320be8e1121231c2205f8812eb124
mutukuian <mutukui940@gmail.com>
2024-09-28 13:41:09 +0300

    Implement terms and condition checkbox

--- app/src/main/java/com/gdscandroid/SignUpScreen.kt
+++ app/src/main/java/com/gdscandroid/SignUpScreen.kt
@@ -1,5 +1,6 @@
 package com.gdscandroid
 
+import android.app.Activity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,6 +13,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -19,26 +21,54 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.gdscandroid.ui.theme.DignifyBackground
-import com.gdscandroid.ui.theme.DignifyGreen
+import androidx.core.view.WindowCompat
+import com.gdscandroid.ui.theme.GdscBackground
+import com.gdscandroid.ui.theme.GdscGreen
 
 @Composable
 fun SignUpScreen() {
 
+    val context = LocalContext.current
 
     val nameState = remember { mutableStateOf("") }
+    val phoneState = remember { mutableStateOf("") }
+    val aliasState = remember { mutableStateOf("") }
 
     val isNameValid = remember { mutableStateOf(true) }
+    val isPhoneValid = remember { mutableStateOf(true) }
+    val isAliasValid = remember { mutableStateOf(true) }
+    val isTermsChecked = remember { mutableStateOf(false) }
 
     fun validateInputs(): Boolean {
         val isNameValidResult = nameState.value.isNotBlank()
+        val isPhoneValidResult = phoneState.value.length == 12 && phoneState.value.startsWith("254")
+        val isAliasValidResult = aliasState.value.isNotBlank()
+
         isNameValid.value = isNameValidResult
-        return isNameValidResult
+        isPhoneValid.value = isPhoneValidResult
+        isAliasValid.value = isAliasValidResult
+
+        return isNameValidResult && isPhoneValidResult && isAliasValidResult && isTermsChecked.value
+    }
+
+
+
+    LaunchedEffect(Unit) {
+        // Set the system bars color to match the app's background
+        val window = (context as Activity).window
+        window.statusBarColor = GdscBackground.toArgb()
+        window.navigationBarColor = GdscBackground.toArgb()
+        WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars =
+            false
+        WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightNavigationBars =
+            false
     }
 
     Scaffold { innerPadding ->
@@ -51,8 +81,8 @@ fun SignUpScreen() {
                     Brush.linearGradient(
                         colors =
                         listOf(
-                            DignifyGreen,
-                            DignifyBackground,
+                            GdscGreen,
+                            GdscBackground,
                         ),
                         start = androidx.compose.ui.geometry.Offset(1000f, 1000f),
                         end = androidx.compose.ui.geometry.Offset(900f, 0f),
@@ -108,6 +138,29 @@ fun SignUpScreen() {
                         )
                     }
 
+                    PhoneNumberField(phoneState, isPhoneValid.value)
+
+                    if (!isPhoneValid.value) {
+                        Text(
+                            text = "Phone number must be 12 characters and start with 254",
+                            color = Color.Red,
+                            style = MaterialTheme.typography.bodySmall,
+                        )
+                    }
+
+                    AliasField(aliasState, isAliasValid.value)
+                    if (!isAliasValid.value) {
+                        Text(
+                            text = "Alias cannot be empty",
+                            color = Color.Red,
+                            style = MaterialTheme.typography.bodySmall,
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.weight(0.4f))
+
+                    TermsCheckbox(isTermsChecked)
+
                     Spacer(modifier = Modifier.weight(3f))
 
 
@@ -127,6 +180,33 @@ fun UserNameField(
         value = nameState.value,
         onValueChange = { newText -> nameState.value = newText },
         label = "Name",
+        placeholder = "GDSCTeam",
+    )
+}
+
+
+@Composable
+fun PhoneNumberField(
+    phoneNumberState: MutableState<String>,
+    isValid: Boolean,
+) {
+    CustomOutlinedTextField(
+        value = phoneNumberState.value,
+        onValueChange = { newText -> phoneNumberState.value = newText },
+        label = "Phone Number",
+        placeholder = "254 700 000 000",
+    )
+}
+
+@Composable
+fun AliasField(
+    aliasState: MutableState<String>,
+    isValid: Boolean,
+) {
+    CustomOutlinedTextField(
+        value = aliasState.value,
+        onValueChange = { newText -> aliasState.value = newText },
+        label = "Alias",
         placeholder = "Guardian Angel",
     )
 }
---
+++ app/src/main/java/com/gdscandroid/TermsCheckBox.kt
@@ -0,0 +1,66 @@
+package com.gdscandroid
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TermsCheckbox(
+    isChecked: MutableState<Boolean>,
+) {
+    Row(
+        modifier =
+        Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        Checkbox(
+            checked = isChecked.value,
+            onCheckedChange = { isChecked.value = it },
+        )
+
+        Spacer(modifier = Modifier.size(8.dp))
+
+        val termsText = "I accept Terms & Conditions"
+        val annotatedString =
+            AnnotatedString.Builder(termsText).apply {
+                addStyle(
+                    style =
+                    SpanStyle(
+                        color = Color.Black,
+                        textDecoration = TextDecoration.Underline,
+                    ),
+                    start = 9,
+                    end = termsText.length,
+                )
+            }.toAnnotatedString()
+
+        ClickableText(
+            text = annotatedString,
+            onClick = { offset ->
+                if (offset >= 9 && offset <= termsText.length) {
+                    // Handle the click event on "terms and conditions"
+                   // navController.navigate(Screens.TermsScreen.route)
+                }
+            },
+            style = MaterialTheme.typography.bodyMedium.copy(color = Color.White),
+        )
+    }
+}
